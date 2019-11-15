56815560e54c2c89fc5f3ca21cee5afc07a115bb
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-11-14 22:14:10 -0500

    Update sum.js

--- src/sum.js
+++ src/sum.js
@@ -3,7 +3,20 @@ const readlineSync = require("readline-sync");
 const MIN = Number.MIN_SAFE_INTEGER;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-let lowerBound = Number(readlineSync.question("\nLower Bound: "));
-let upperBound = Number(readlineSync.question("Upper Bound: "));
+let lower = Number(readlineSync.question("\nLower Bound: "));
+let upper = Number(readlineSync.question("Upper Bound: "));
+let sum = 0;
 
-while (lowerBound >= upperBound)
+while (lower >= upper || Number.isNaN(lower) || Number.isNaN(upper) || MIN > lower || MAX < upper || !Number.isInteger(lower) || !Number.isInteger(upper)) {
+  lower = Number(readlineSync.question("Lower Bound: "));
+  upper = Number(readlineSync.question("Upper Bound: "));
+}
+
+for (let i = lower; i <= upper; i++) {
+  if (i % 2 === 0) {
+    sum += i;
+  }
+}
+
+sum = sum.toLocaleString('en');
+console.log("\n" + sum + ".");
