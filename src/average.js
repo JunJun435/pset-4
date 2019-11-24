0f42ae14c6490e499dd29141b4d44a1aacaf00e3
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-11-24 12:58:46 -0500

    Create average.js

---
+++ src/average.js
@@ -0,0 +1,21 @@
+const readlineSync = require("readline-sync");
+
+const MIN = 0;
+const MAX = Number.MAX_SAFE_INTEGER;
+
+console.log("");
+let input = 0;
+let sum = 0;
+let amount = 0;
+do {
+  input = Number(readlineSync.question("Non-negative integer: "));
+  if (input >= MIN && input < MAX && Number.isInteger(input)) {
+    sum = sum + input;
+    amount = amount + 1;
+  }
+} while (input > 0);
+
+let average = sum / amount;
+average = average.toLocaleString('en', {minimumFractionDigits: 3, maximumFractionDigits: 3});
+
+console.log("\n" + average + ".");
