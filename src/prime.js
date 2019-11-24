d8f89fb3c071971c512ab3e9114d4938257a8531
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-11-24 13:02:32 -0500

    finished

--- src/prime.js
+++ src/prime.js
@@ -9,10 +9,10 @@ do {
   input = Number(readlineSync.question("Non-negative integer: "));
 } while (input < MIN || input > MAX || !Number.isInteger(input));
 
-let prime = true;
+let primeNum = true;
 for (let i = 2; i < input; i = i + 1) {
   if (input % i == 0) {
-    prime = false;
+    primeNum = false;
   }
 }
 
@@ -20,7 +20,7 @@ if (input == 0) {
   console.log("\nNot prime.");
 } else if (input == 1) {
   console.log("\nNot prime.");
-} else if (prime == true) {
+} else if (primeNum == true) {
   console.log("\nPrime.")
 } else {
   console.log("\nNot prime.")
