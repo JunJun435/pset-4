20afdfd15939a2f7128bda09b7e29ae8184d6c95
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-11-24 18:44:28 -0500

    finished

--- src/mario.js
+++ src/mario.js
@@ -1,6 +1,32 @@
-const readlineSync = require("readline-sync");
+const readlineSync = require('readline-sync');
 
-const minInteger = 1
-const maxInteger = 24
+const MAX = 24;
+const MIN = 1;
 
-let height = Number(readlineSync.question("\nHeight: "));
+let mario = readlineSync.question('\nHeight: ');
+
+while (mario < 1 || mario > 24) {
+  mario = readlineSync.question("Height: ");
+
+  mario = parseInt(height);
+
+  if (mario > 0 && mario <= 24) {
+
+    for (i = 1; i <= mario; i++) {
+      var spaces = mario - i;
+      var tags = i + 1;
+
+      var space = ('');
+      var hashtag = ('');
+
+      for (j = 1; j <= spaces; j++) {
+        space += ' ';
+      }
+      for (j = 1; j <= tags; j++) {
+        hashtag += '#';
+
+      }
+      console.log(space + hashtag);
+    }
+  }
+}
