39ca53306edc4592fc0049348c328e4ca7ec4858
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-11-24 12:45:43 -0500

    finally done with rev

--- src/reverse.js
+++ src/reverse.js
@@ -3,8 +3,22 @@ const readlineSync = require("readline-sync");
 const MIN = 1;
 const MAX = Number.MAX_SAFE_INTEGER;
 
-let positiveInteger = Number(readlineSync.question("\nPositive integer: "));
+console.log("");
+let input = 0;
+do {
+  input = Number(readlineSync.question("Positive integer: "))
+} while (input <= MIN || input >= MAX || input <= 0 || !Number.isInteger(input));
 
-while Number.isNan(positiveInteger) || positiveInteger < MIN || positiveInteger > MAX || Number.isInteger(positiveInteger) {
-  positiveInteger = Number(readlineSync.question("\nPositive integer: "));
-}
+let number = 0;
+let revNumber = "";
+do {
+  number = input % 10;
+  input = Math.floor(input / 10);
+  if (input > 0) {
+    revNumber = revNumber + number + ", ";
+  } else {
+    revNumber = revNumber + number;
+  }
+} while (input > 0);
+
+console.log("\n" + revNumber + ".");
